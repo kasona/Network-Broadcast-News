35dd9273f8fabdce667de87e83068bebba94d9da
kasi <kasionaga@gmail.com>
2015-10-16 12:26:17 -1000

    Working on username

--- client1.js
+++ client1.js
@@ -3,18 +3,22 @@ var net = require('net');
 
 // Creating a Client
 var client = new net.Socket();
+var HOST = '0.0.0.0';
+var PORT = '6969';
 
 //client.connect(port, host, callback)
-client.connect(6969, '0.0.0.0', function() {
+client.connect(PORT, HOST, function() {
   console.log('Connected');
-  client.write('This is Client! ');
+  client.write('This is Client1! ');
 
   // Takes in data and writes it to server
+  // 'data' is the event on is looking/listening for
   process.stdin.on('data', function(chunk) {
+    client.write('Client 1: ');
     client.write(chunk);
   });
-});
 
+});
 
 client.on('data', function(data) {
   console.log('Received: ' + data);
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 // Requires
 var net = require('net');
+
+
 // Variables ( easier to edit, if needed )
 var HOST = '0.0.0.0';
 var PORT = '6969';
 
+// Object to store clients (keep track of clients when connecting)
+var clients = [];
 
 // Creating a server with net
 // c is socket connection, can have many sockets
 var server = net.createServer(function(socket) {
+  // ask for a username
+  clients.push(socket);
 
   // Start of connection
   console.log('Server Connected: ' + HOST + ':' + PORT);
-  socket.write('this has to be dynamic');
 
   // Listening for data event
   socket.on('data', function(chunk) {
-  //process is apart of node, stdout displays to terminal
+    console.log('received:', chunk);
+    sendAll(chunk);
+    // create a sendAll(); using the client array, stdout to all clients in [];
+    //process is apart of node, stdout displays to terminal
     process.stdout.write(chunk);
+
   });
 
   // End of connection
@@ -27,3 +36,23 @@ var server = net.createServer(function(socket) {
 
 server.listen(PORT, HOST);
 console.log('Listening on .. Host:', HOST, 'Port:', PORT);
+
+// ================ Broadcast =====================
+function sendAll (data) {
+  for ( var i = 0; i < clients.length; i++) {
+    clients[i].write(data);
+  }
+}
+
+// =============== Ask for Username ==============
+function username () {
+  socket.on('data', function(chunk) {
+    console.log('Please enter a username:');
+    //Store the first input as user name
+    console.log('Is this correct? y / n  ' + username);
+    // If client's input ( to lowercase ) == y then store username into array
+    // Put username infront of users chunk
+    // If client's input ( to lowercase ) == n then Ask user to re-enter username, loop to check
+    // If client's input ( to lowercase ) !== y && n then err, user needs to re-enter y or n
+  });
+}
